61a7bfa1943a93604767d8f566f8555cdb892d00
kkleindev <kleinkevin.lu@gmail.com>
2017-09-15 17:06:39 +0200

    Code refactor

--- analysis.py
+++ analysis.py
@@ -1,15 +1,14 @@
-from __future__ import print_function
+import datetime
 import httplib2
 import matplotlib
 import matplotlib.pyplot as plt
 import os
 
 from apiclient import discovery
+from collections import defaultdict
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
-import datetime
 
 try:
     import argparse
@@ -22,7 +21,7 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
-
+START_TIME = datetime.datetime(2016, 9, 10).isoformat()
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -55,10 +54,10 @@ def get_credentials():
 def descriptionToDistance(description):
     return float(description.split('km')[0])
 
-def processDate(date, firstDate):
-    diff = int(date) - firstDate
-    result = diff // 7
-    return result
+def dateToWeek(date, firstDate):
+    """Returns the week number of the date from firstDate.
+    """
+    return (int(date) - int(firstDate)) // 7
 
 def main():
     """TODO(kkleindev)
@@ -67,79 +66,68 @@ def main():
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    timeStart = datetime.datetime(2016, 9, 10).isoformat() + 'Z'
-    timeNow = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    # 'Z' indicates UTC time
+    timeStart = START_TIME + 'Z'
+    timeNow = datetime.datetime.utcnow().isoformat() + 'Z'
     print('Getting the upcoming 10 events')
     eventsResult = service.events().list(
-        calendarId='primary', timeMin=timeStart, timeMax = timeNow, maxResults=100000, singleEvents=True,
-        orderBy='startTime').execute()
+        calendarId='primary', timeMin=timeStart, timeMax = timeNow,
+        maxResults=100000, singleEvents=True, orderBy='startTime').execute()
     events = eventsResult.get('items', [])
 
-    # meta stats
     print('Total number of events before filterting: ' + str(len(events)))
 
     descriptionFilteredEvents = []
     colorFilteredEvents = []
     intersection = []
     for event in events:
-        #if 'colorId' in event and event['colorId'] == '4':
         if 'summary' in event and event['summary'] == 'Running':
             descriptionFilteredEvents.append(event)
         if 'colorId' in event and event['colorId'] == '4':
             colorFilteredEvents.append(event)
-        if 'summary' in event and event['summary'] == 'Running' and 'colorId' in event and event['colorId'] == '4':
-            intersection.append(event)
-    print('Total number of events with description Running: ' + str(len(descriptionFilteredEvents)))
-    print('Total number of events with color Flamingo: ' + str(len(colorFilteredEvents)))
 
-#    intersection = descriptionEventSet.intersection(colorEventSet)
+    print('Total number of events with description Running: ' +
+        str(len(descriptionFilteredEvents)))
+    print('Total number of events with color Flamingo: ' +
+        str(len(colorFilteredEvents)))
 
-    print('Description && Color / Description: ' + str(len(intersection)/len(descriptionFilteredEvents)))
-    print('Description && Color / Color: ' + str(len(intersection)/len(colorFilteredEvents)))
-
-    dates = []
-    distances = []
-
+    # Compute the running distance per week.
+    sportPerWeek = defaultdict(lambda: 0)
+    firstDate = matplotlib.dates.datestr2num(
+        [descriptionFilteredEvents[0]['start']['dateTime']])
     for event in descriptionFilteredEvents:
         if 'description' in event:
-            dates.append(event['start']['dateTime'])
-            distances.append(descriptionToDistance(event['description']))
+            sportPerWeek[dateToWeek(
+                matplotlib.dates.datestr2num([event['start']['dateTime']]),
+                firstDate)] += descriptionToDistance(event['description'])
 
-    dates = matplotlib.dates.datestr2num(dates)
-    weekDates = []
-    firstDate = ''
-    for date in dates:
-        if firstDate == '':
-           firstDate = date
-        weekDates.append(processDate(date, firstDate))
-
-    pointer = 0
-
-    aggregatedWeekDates = []
-    aggregatedDistances = []
-
-    currentDistance = 0
-    currentWeek = weekDates[0]
-
-    while pointer < len(distances):
-        if weekDates[pointer] == currentWeek:
-            currentDistance += distances[pointer]
-        else:
-            aggregatedWeekDates.append(currentWeek)
-            aggregatedDistances.append(currentDistance)
-            currentWeek = weekDates[pointer]
-            currentDistance = distances[pointer]
-        pointer += 1
-
-    aggregatedWeekDates.append(currentWeek)
-    aggregatedDistances.append(currentDistance)
+    aggregatedWeekDates, aggregatedDistances = zip(*sportPerWeek.items())
 
     averageDistance = sum(aggregatedDistances)/len(aggregatedDistances)
+    print ('Average distance per week: ' + str(averageDistance))
 
     plt.axhline(y=averageDistance, color='r', linestyle='-')
-    plt.scatter(aggregatedWeekDates,aggregatedDistances)
+    plt.scatter(aggregatedWeekDates, aggregatedDistances)
     plt.show()
 
+    # Compute the number of sport events per week.
+    sportPerWeek = defaultdict(lambda: 0)
+    firstDate = matplotlib.dates.datestr2num(
+        [colorFilteredEvents[0]['start']['dateTime']])
+    for event in colorFilteredEvents:
+        sportPerWeek[dateToWeek(
+            matplotlib.dates.datestr2num([event['start']['dateTime']]),
+            firstDate)] += 1
+
+    aggregatedWeekDates, aggregatedSportCount = zip(*sportPerWeek.items())
+
+    averageSportsCount = len(colorFilteredEvents) / len(aggregatedWeekDates)
+    print ('Average number of sports events per week: ' +
+        str(averageSportsCount))
+
+    plt.axhline(y=averageSportsCount, color='r', linestyle='-')
+    plt.scatter(aggregatedWeekDates, aggregatedSportCount)
+    plt.show()
 
 if __name__ == '__main__':
     main()
