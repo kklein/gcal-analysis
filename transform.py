683b3221bc75fe2dddf90aedf74f89906a885c04
Kevin Klein <kleinkevin.lu@gmail.com>
2020-12-27 17:54:36 +0100

    Include hourly information.

--- transform.py
+++ transform.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 def get_dataframe(events):
     d = {
-        "date": map(lambda event: event["start"]["dateTime"], events),
-        "distance": map(lambda event: event["description"].split("km")[0], events),
+        "date_string": map(lambda event: event["start"]["dateTime"], events),
+        "distance": map(lambda event: event.get("description", "").split("km")[0], events),
     }
     df = pd.DataFrame(data=d)
-    df["date"] = df["date"].str[:10]
-    df["date"] = pd.to_datetime(df["date"])
+    df["date"] = pd.to_datetime(df["date_string"].str[:16])
     df["week"] = df["date"].dt.isocalendar().week
     df["month"] = df["date"].dt.month
     df["year"] = df["date"].dt.year
     df["distance"] = pd.to_numeric(df["distance"])
+    df["hour"] = df["date"].dt.hour
     return df
